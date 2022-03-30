fbc9ae80dca620182713290dcd2a49cdb2410805
Labib hasan Rana <82722829+Coder0Labib@users.noreply.github.com>
2022-03-30 14:44:38 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<!-- I have three old hp monitors, not new. I want to sell it on my own website. That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. My monitors have a 15.6 inch display. My monitor’s VGA cable status, yes, I will sell it for 8000 tk.
+<!-- I have three old hp monitors, not new. 
+I want to sell it on my own website. 
+That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. 
+My monitors have a 15.6 inch display. My monitor’s VGA cable status, yes, I will sell it for 8000 tk.
  -->
 
 <?php 
