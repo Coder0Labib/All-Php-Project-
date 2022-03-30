5c081b67cea6c86d4b9cf0bcd48bdd340b98352a
Coder0Labib <halalulislamsohel@gmail.com>
2022-03-30 14:43:19 +0600

    20/04/2022

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
+<!-- I have three old hp monitors, not new. I want to sell it on my own website. That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. My monitors have a 15.6 inch display. My monitor’s VGA cable status, yes, I will sell it for 8000 tk.
+ -->
+
+<?php 
+$TheTotalNumberOfMonitor = 3;// Datatype is integer.
+$name = Hp Monitor; // DataType is string.
+$MonitorDisplaySize = 15.6; // Datatype is Double/float.
+$VGACableStatus = ture; //Datatype is Boolean.
+$SellPrice = =8000;//datatype is integer.
 
 ?>
